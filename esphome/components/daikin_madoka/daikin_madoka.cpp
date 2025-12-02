a4322795ba245b2b55669d5fec96cde459c3ffbd
Rolandas Razma <rolandas@razma.lt>
2025-12-02 22:03:17 +0000

    add log

--- esphome/components/daikin_madoka/daikin_madoka.cpp
+++ esphome/components/daikin_madoka/daikin_madoka.cpp
@@ -153,8 +153,9 @@ void DaikinMadoka::gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_c
   }
 }
 
-void DaikinMadoka::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                                       esp_ble_gattc_cb_param_t *param) {
+void DaikinMadoka::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
+  ESP_LOGD(TAG, "GATTc event: %d", event);
+  
   switch (event) {
     case ESP_GATTC_DISCONNECT_EVT: {
       this->node_state = espbt::ClientState::IDLE;  // ??
@@ -195,6 +196,7 @@ void DaikinMadoka::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
     default:
       break;
   }
+
 }
 
 void DaikinMadoka::update() {
